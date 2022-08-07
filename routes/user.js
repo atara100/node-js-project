5b1a103d413d4d9b0f54b9f15ecd9c7432e722c1
Atara Levi <98110096+atara100@users.noreply.github.com>
2022-08-07 15:53:36 +0300

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const userSchema = require('../validators/user');
+const UserModel = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const _ = require('lodash');
+const chalk=require('chalk');
+
+
+router.post("/create" ,createRequest);
+
+async function createRequest(req, res) {
+
+        const { error, value } = userSchema.newUser.validate(req.body);
+        const user = value;
+        if (error) {
+            console.log(chalk.red(error));
+            res.status(400).send(error)
+        }
+        else {
+            try {
+                const result = await UserModel.find({email:user.email});
+                if (result.length > 0) {
+                    res.status(400).send("User already exists...🙄")
+                }
+                else {
+                    try {
+                     const savedUser = await saveUser(user);
+                     res.status(200).send(savedUser);
+                    }
+                    catch (err) {
+                        res.status(400).send(err);
+                    }
+                    
+                }
+            } catch (err) {
+                res.status(400).send(err)
+            }
+        
+        }
+    }
+    
+ function saveUser(user){
+    return new Promise(async (resolve, reject) => {
+        try {
+            user.password = await bcrypt.hash(user.password, saltRounds);
+            const savedUser = await new UserModel(user).save();
+            resolve(_.pick(savedUser,['name','email','_id']));
+       } catch (err) {
+           reject (err);
+       }
+    })
+    }
+
+    router.post("/auth" ,login);
+
+    async function login(req,res){
+        const { error, value } = userSchema.auth.validate(req.body);
+        const user = value;
+        if (error) {
+            res.status(400).send(error)
+        }
+        else{
+            try{
+                const userModel = await UserModel.findOne({email:user.email});
+                if (!userModel) { 
+                    res.status(400).send("Username or password wrong 👿");
+                    return;
+                }
+                const isAuth = await userModel.checkPassword(user.password);
+                res.status(200).send(isAuth);
+            } catch (err) {
+                res.status(400).send(err)
+            }
+        }
+    }
+
+module.exports = router;
